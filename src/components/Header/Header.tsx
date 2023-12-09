0d3384aca72d7d81e045456382d10d3f709919a7
Marcelo Leal <andradefj13@hotmail.com>
2023-12-09 16:31:36 -0300

    updateCSS
    atualização do BG, mudança visual no header, e inplementação de efeito e função para a barra de nav em telas menores

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,40 @@
 import { Link } from "react-router-dom";
 import './header.css'
 import logoMarca from './img/logo-marca.gif'
+import { useState } from "react";
 
 export default function Header() {
+    const [headerContainer, setHeaderContainer]= useState('header-container')
+    const [classNav, setClassNav] = useState('nav-container')
+    const [bodyHeader, setBodyHeader] = useState('body-header')
+    function handleClick(){
+        if(headerContainer === 'header-container'){
+            setHeaderContainer('header-container-minimized')
+            setClassNav('nav-container-minimized')
+            setBodyHeader('body-header-minimized')
+        }
+        else{
+            setHeaderContainer('header-container')
+            setClassNav('nav-container')
+            setBodyHeader('body-header')
+        }
+    }
     return (
-        <header>
-            <img src={logoMarca} alt="Bio-Corpo-Academia" id="logo-marca" />
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/about">Contato</Link>
-                <button></button>
-            </nav>
+        <header className={bodyHeader}>
+            <div className={headerContainer}>
+                <Link to="/" className="logo">
+                    <img src={logoMarca} alt="logo marca" />
+                </Link>
+                <nav className={classNav}>
+                    <Link to="/">Home</Link>
+                    <Link to="/">Sobre</Link>
+                    <Link to="/">Equipe</Link>
+                    <Link to="/">Serviços</Link>
+                    <Link to="/">Fotos</Link>
+                    <Link to="/">Contato</Link>
+                </nav>
+            </div>
+            <button onClick={()=>handleClick()}>⇊</button>
         </header>
     )
 }
