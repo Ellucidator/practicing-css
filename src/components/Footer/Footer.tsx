d607b3d9956e5e25cbf202d980e2a7b369799c43
Marcelo Leal <andradefj13@hotmail.com>
2023-12-10 17:25:41 -0300

    updateCSS
    Correção no posicionamento do elementos e implementação do footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import './footer.css'
+export default function Footer(){
+    
+    return(
+        <footer>
+            <address>
+                <p>Endereço: Rua dos Bobos, 0</p>
+                <p>Cidade: Cidade</p>
+                <p>CEP: 00000-000</p>
+                <p>Telefone: (00) 0000-0000</p>
+                <p>Email: QvW7A@example.com</p>
+            </address>
+        </footer>
+    )
+}
--- src/layouts/HeaderLayout.tsx
+++ src/layouts/HeaderLayout.tsx
@@ -1,15 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
 
 export function HeaderLayout() {
     return (
         <>
             <Header />
             <Outlet />
-            
-            <footer>
-
-            </footer>
+            <Footer />
         </>
     )
 }
