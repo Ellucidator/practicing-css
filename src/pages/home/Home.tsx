e2dbf8eb9c1c7f338204df6553d77b703d4e3e47
Marcelo Leal <andradefj13@hotmail.com>
2023-12-10 19:00:54 -0300

    updateCSS
    Implementação da segunda parte da pagina home, e estilização

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,10 +4,25 @@ export function Home(){
 
     return (
         <div className="home">
-            <section className="home-initial">
-                <h1 className='title-home-01'>Junte-se a Familia</h1>
-                <h2 className='title-home-01-1'>Bio Corpo</h2>
-                <button className='button-home'>Matricule-se</button>
+            <section className="home-element-01">
+                <h1 className='title-element-01'>Junte-se a Familia</h1>
+                <h1 className='title-element-01-1'>Bio Corpo</h1>
+                <button className='button-element-01'>Matricule-se</button>
+            </section>
+            <h1 className='title-modalites'>Modalidades</h1>
+            <section className='home-element-02'>
+                <div className='conatiner-modalites'>
+                    <h1 className='title-element-02-01'>Musculação</h1>
+                    <p className='text-element-02-01'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                    Porro rerum commodi sit atque nemo nobis ratione natus maxime nulla. Provident.</p>
+                </div>
+                <div className='conatiner-modalites functional'>
+                <h1 className='title-element-02-01'>Funcional</h1>
+                    <p className='text-element-02-01'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                    Porro rerum commodi sit atque nemo nobis ratione natus maxime nulla. Provident.</p>
+                </div>
+                <div className='conatiner-modalites'></div>
+                <div className='conatiner-modalites'></div>
             </section>
         </div>
     )
